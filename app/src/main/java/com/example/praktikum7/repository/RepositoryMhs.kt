614f1780720aeba1a02fbdee329c8cca671bc804
granityo <frhnspensa@gmai.com>
2024-12-17 11:46:34 +0700

    Mengambil semua data Mahasiswa dari database

--- app/src/main/java/com/example/praktikum7/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/praktikum7/repository/RepositoryMhs.kt
@@ -1,7 +1,14 @@
 package com.example.praktikum7.repository
 
 import com.example.praktikum7.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 interface RepositoryMhs {
+    //interface ini operasinya harus sesuai dengan yang di dao
     suspend fun insertMhs(mahasiswa: Mahasiswa)
+
+    //getAllMhs
+    fun getAllMhs(): Flow<List<Mahasiswa>>
+
+
 }
