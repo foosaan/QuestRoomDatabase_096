33ecc57a914424b2ad3936bf9769fdd65eedee1f
granityo <frhnspensa@gmai.com>
2024-12-17 12:37:50 +0700

    perbaikan

---
+++ app/src/main/java/com/example/praktikum7/ui/view/UpdateMhsView.kt
@@ -0,0 +1,87 @@
+package com.example.praktikum7.ui.view
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+@Composable
+fun UpdateMhsView(
+    onBack: () -> Unit,
+    onNavigate: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: UpdateMhsViewModel = viewModel(factory = PenyediaViewModel.Factory) //Inisialisasi ViewModel
+) {
+    val uiState = viewModel.updateUIState //ambil UI state dari viewModel
+    val snackbarHostState = remember { SnackbarHostState() } //snackbar state
+    val coroutineScope = rememberCoroutineScope()
+
+    //obeservasi perubahan snackbarMessage
+    LaunchedEffect(uiState.snackBarMessage) {
+        println("LaunchedEffect triggered")
+        uiState.snackBarMessage?.let { message ->
+            println("Snackbar message received: $message")
+            coroutineScope.launch {
+                println("Launching coroutine for snackbar")
+                snackbarHostState.showSnackbar(
+                    message = message,
+                    duration = SnackbarDuration.Long
+                )
+                viewModel.resetSnackBarMessage()
+            }
+        }
+    }
+
+    Scaffold (
+        modifier = modifier,
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
+        //tempatkan snackbar di scaffold
+        topBar = {
+            CstTopAppBar(
+                judul ="Edit Mahasiswa",
+                showBackButton = true,
+                onBack = onBack,
+            )
+        }
+    ) { padding ->
+        Column (
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+                .padding(16.dp)
+        ){
+
+            //isibody
+            InsertBodyMhs(
+                uiState = uiState,
+                onValueChange = { updatedEvent ->
+                    viewModel.updateState(updatedEvent) //update state di viewmodel
+                },
+                onClick = {
+                    coroutineScope.launch {
+                        if (viewModel.validateFields() ) {
+                            viewModel.updateData()
+                            delay(600)
+                            withContext(Dispatchers.Main) {
+                                onNavigate() //navigasi di main thread
+                            }
+                        }
+                    }
+                }
+            )
+        }
+    }
+}
--- app/src/main/java/com/example/praktikum7/ui/viewmodel/MahasiswaViewModel.kt
+++ app/src/main/java/com/example/praktikum7/ui/viewmodel/MahasiswaViewModel.kt
@@ -1,93 +1,18 @@
-package com.example.myapplication.ui.theme.Viewmodel
+package com.example.praktikum7.ui.viewmodel
 
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.example.myapplication.data.entity.Mahasiswa
-import com.example.myapplication.repository.RepositoryMhs
-import kotlinx.coroutines.launch
-
-class MahasiswaViewModel(private val repositoryMhs: RepositoryMhs) : ViewModel(){
-
-    var uiState by mutableStateOf(MhsUIState())
-    fun updateState(mahasiswaEvent: MahasiswaEvent){
-        uiState = uiState.copy(
-            mahasiswaEvent = mahasiswaEvent,
-        )
-    }
-
-    private fun validateFields(): Boolean{
-        val event = uiState.mahasiswaEvent
-        val errorState = FormErrorState(
-            nim = if (event.nim.isNotEmpty()) null else "NIM Tidak Boleh Kosong",
-            nama = if (event.nama.isNotEmpty()) null else "Nama Tidak Boleh Kosong",
-            jenisKelamin = if (event.jenisKelamin.isNotEmpty()) null else "Jenis Kelamin Tidak Boleh Kosong",
-            alamat = if (event.alamat.isNotEmpty()) null else "Alamat Tidak Boleh Kosong",
-            kelas = if (event.kelas.isNotEmpty()) null else "Kelas Tidak Boleh Kosong",
-            angkatan = if (event.angkatan.isNotEmpty()) null else "Angkatan Tidak Boleh Kosong",
-        )
-
-        uiState = uiState.copy(isEntryValid = errorState)
-        return errorState.isValid()
-
-
-    }
-
-    fun saveData(){
-        val currentEvent = uiState.mahasiswaEvent
-
-        if (validateFields()) {
-            viewModelScope.launch {
-                try {
-                    repositoryMhs.insertMhs(currentEvent.toMahasiswaEntity())
-                    uiState= uiState.copy(
-                        snackbarMessage = "Data Berhasil disimpan",
-                        mahasiswaEvent = MahasiswaEvent(),
-                        isEntryValid = FormErrorState()
-                    )
-                } catch (e: Exception){
-                    uiState = uiState.copy(
-                        snackbarMessage = "Data gagal disimpan"
-                    )
-                }
-            }
-        } else {
-            uiState = uiState.copy(
-                snackbarMessage = "Input Tidak valid. Periksa kembali data anda."
-            )
-        }
-    }
-
-    fun resetSnackBarMessage(){
-        uiState = uiState.copy(snackbarMessage = null)
-    }
-}
-
-data class MhsUIState(
-    val  mahasiswaEvent: MahasiswaEvent = MahasiswaEvent(),
-    val  isEntryValid: FormErrorState = FormErrorState(),
-    val  snackbarMessage: String? = null
-)
+import com.example.praktikum7.data.entity.Mahasiswa
 
 data class FormErrorState(
-    val nim: String? = null,
-    val nama: String? = null,
-    val jenisKelamin: String? = null,
-    val alamat: String? = null,
-    val kelas: String? = null,
-    val angkatan: String? = null,
-){
-    fun isValid(): Boolean{
-        return nim == null && nama == null && jenisKelamin == null &&
-                alamat == null && kelas == null && angkatan == null
-    }
-}
+    val nim: String? = nulll,
+    val nama: String? = nulll,
+    val nim: String? = nulll,
+    val nim: String? = nulll,
+    val nim: String? = nulll,
+    val nim: String? = nulll,
+)
 
-
-
-fun MahasiswaEvent.toMahasiswaEntity(): Mahasiswa = Mahasiswa(
+//menyimpan input form kedalam entity
+fun MahsiswaEvent.toMahasiswaEntity(): Mahasiswa=Mahasiswa(
     nim = nim,
     nama = nama,
     jenisKelamin = jenisKelamin,
@@ -96,11 +21,13 @@ fun MahasiswaEvent.toMahasiswaEntity(): Mahasiswa = Mahasiswa(
     angkatan = angkatan
 )
 
-data class MahasiswaEvent(
-    val nim: String = "",
-    val nama: String = "",
-    val jenisKelamin: String = "",
-    val alamat: String = "",
-    val kelas: String = "",
-    val angkatan: String = ""
+//data class variable tang menyimpan data imput form
+data class MahsiswaEvent(
+    val nim: String ="",
+    val nama: String ="",
+    val jenisKelamin: String ="",
+    val alamat: String ="",
+    val kelas: String ="",
+    val angkatan: String ="",
 )
+
